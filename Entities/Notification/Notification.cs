b4b6b187ada1df6890e5b7a5f776a85745ca2551
Anderson Zion <andersonzion@hotmail.com>
2020-11-10 16:47:40 -0300

    Criação dos Métodos AssertArgument 1- AssertArgumentEquals

--- Entities/Notification/Notification.cs
+++ Entities/Notification/Notification.cs
@@ -14,5 +14,26 @@ namespace Entities.Notification
         public string MyProperty { get; set; }
         public string MyMessage { get; set; }
         public List<Notification> MyNotifies { get; set; }
+
+
+        public bool AssertArgumentEquals( object object1, object object2, string strProperty)
+        {
+            if (object1.Equals(object2))
+            {
+                return true;
+            }
+            else
+            {
+                MyNotifies.Add(new Notification
+                {
+                    MyMessage = string.Format(""),
+                    MyProperty = strProperty
+                });
+                return false;
+            }
+
+        }
+
+
     }
 }
